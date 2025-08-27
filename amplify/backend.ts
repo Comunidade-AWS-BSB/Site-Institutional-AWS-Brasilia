4a5e055d2edb56ac0b725f8567012e9740b32142
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-08-27 16:51:31 -0300

    feat(auth): recria pool com schema final

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,64 +1,64 @@
 // amplify/backend.ts
 import { defineBackend } from '@aws-amplify/backend'
-// import { auth } from './auth/resource'
+import { auth } from './auth/resource'
 import { data } from './data/resource'
 import { storage } from './storage/resource'
 
-// import * as cognito from 'aws-cdk-lib/aws-cognito'
-// import * as acm from 'aws-cdk-lib/aws-certificatemanager'
-// import * as route53 from 'aws-cdk-lib/aws-route53'
-// import * as targets from 'aws-cdk-lib/aws-route53-targets'
+import * as cognito from 'aws-cdk-lib/aws-cognito'
+import * as acm from 'aws-cdk-lib/aws-certificatemanager'
+import * as route53 from 'aws-cdk-lib/aws-route53'
+import * as targets from 'aws-cdk-lib/aws-route53-targets'
 
-const backend = defineBackend({ data, storage })
+const backend = defineBackend({ auth, data, storage })
 
-// // User Pool criado pelo defineAuth
-// const userPool = backend.auth.resources.userPool
+// User Pool criado pelo defineAuth
+const userPool = backend.auth.resources.userPool
 
-// const domainName = 'auth.awsbrasilia.com.br'
-// const zoneName = 'awsbrasilia.com.br'
-// const certArn = process.env.COGNITO_CUSTOM_CERT_ARN!
-// const hostedZoneId = process.env.ROUTE53_ZONE_ID!
+const domainName = 'auth.awsbrasilia.com.br'
+const zoneName = 'awsbrasilia.com.br'
+const certArn = process.env.COGNITO_CUSTOM_CERT_ARN!
+const hostedZoneId = process.env.ROUTE53_ZONE_ID!
 
-// if (certArn && hostedZoneId) {
-//   const cert = acm.Certificate.fromCertificateArn(
-//     backend.stack,
-//     'AuthCustomCert',
-//     certArn
-//   )
+if (certArn && hostedZoneId) {
+  const cert = acm.Certificate.fromCertificateArn(
+    backend.stack,
+    'AuthCustomCert',
+    certArn
+  )
 
-//   // Domínio customizado do Cognito (Hosted UI)
-//   const userPoolDomain = new cognito.UserPoolDomain(
-//     backend.stack,
-//     'AuthCustomDomain',
-//     {
-//       userPool,
-//       customDomain: {
-//         domainName,
-//         certificate: cert,
-//       },
-//     }
-//   )
+  // Domínio customizado do Cognito (Hosted UI)
+  const userPoolDomain = new cognito.UserPoolDomain(
+    backend.stack,
+    'AuthCustomDomain',
+    {
+      userPool,
+      customDomain: {
+        domainName,
+        certificate: cert,
+      },
+    }
+  )
 
-//   const zone = route53.HostedZone.fromHostedZoneAttributes(
-//     backend.stack, 'PrimaryZone',
-//     { hostedZoneId, zoneName }
-//   )
+  const zone = route53.HostedZone.fromHostedZoneAttributes(
+    backend.stack, 'PrimaryZone',
+    { hostedZoneId, zoneName }
+  )
 
 
-//   const recordName = 'auth'
-//   new route53.ARecord(backend.stack, 'AuthAliasA', {
-//     zone,
-//     recordName,
-//     target: route53.RecordTarget.fromAlias(
-//       new targets.UserPoolDomainTarget(userPoolDomain)
-//     ),
-//   })
+  const recordName = 'auth'
+  new route53.ARecord(backend.stack, 'AuthAliasA', {
+    zone,
+    recordName,
+    target: route53.RecordTarget.fromAlias(
+      new targets.UserPoolDomainTarget(userPoolDomain)
+    ),
+  })
 
-//   new route53.AaaaRecord(backend.stack, 'AuthAliasAAAA', {
-//     zone,
-//     recordName,
-//     target: route53.RecordTarget.fromAlias(
-//       new targets.UserPoolDomainTarget(userPoolDomain)
-//     ),
-//   })
-// }
+  new route53.AaaaRecord(backend.stack, 'AuthAliasAAAA', {
+    zone,
+    recordName,
+    target: route53.RecordTarget.fromAlias(
+      new targets.UserPoolDomainTarget(userPoolDomain)
+    ),
+  })
+}
